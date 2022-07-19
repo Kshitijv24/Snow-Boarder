04cd6db6638d3dc1c2a9275bd0acef1e890d6643
Kshitijv24 <kshitijv24@gmail.com>
2022-07-19 11:02:27 +0530

    Triggering our particle effects through script

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem crashEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", 1f);
         }
     }
--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem finishEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", 1f);
         }
     }
